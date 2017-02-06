d0a93863559ba892e66a286f24e2a61f76ae828b
Marcin Wrobel <marcin.wrobel@schibsted.pl>
2017-02-06 15:54:49 +0100

    clarify parameter name

--- src/schibsted/payment/resources/Autopayout.php
+++ src/schibsted/payment/resources/Autopayout.php
@@ -9,9 +9,9 @@ use schibsted\payment\sdk\response\Failure;
  *
  * PMS has these relevant api endpoints:
  *
- * (spidcash)  POST    /api/v{version}/autoPayout
- * (spidcash)  GET     /api/v{version}/autoPayout/{walletId}
- * (spidcash)  DELETE  /api/v{version}/autoPayout/{walletId}
+ * (spidcash)  POST    /api/v{version}/autoPayout body: {"fromPaymentMethodId": 12, "toPaymentMethodId": 77}
+ * (spidcash)  GET     /api/v{version}/autoPayout/{methodId}
+ * (spidcash)  DELETE  /api/v{version}/autoPayout/{methodId}
  */
 class Autopayout extends \schibsted\payment\lib\Resource
 {
